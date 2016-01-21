c4a31aa6397e3cd16459a5359ad32bd5a3fbca92
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-21 14:22:37 -0600

    Removed dummy code

--- IOS_ScrollContentSlider/Home.m
+++ IOS_ScrollContentSlider/Home.m
@@ -41,54 +41,6 @@
     
     // Add loop for set image on scroll
     
-    /*UIImageView *p1 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.scSlide.frame.size.width, 250)];
-    p1.image = [UIImage imageNamed:self.arImages[0]];
-    p1.contentMode = UIViewContentModeScaleToFill;
-    
-    UILabel *lbl1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.scSlide.frame.size.width, 250)];
-    lbl1.text = self.arTitles[0];
-    
-    [self.scSlide addSubview:p1];
-    [self.scSlide addSubview:lbl1];
-    
-    UIImageView *p2 = [[UIImageView alloc] initWithFrame:CGRectMake(self.scSlide.frame.size.width, 0, self.scSlide.frame.size.width, 250)];
-    p2.image = [UIImage imageNamed:self.arImages[1]];
-    p2.contentMode = UIViewContentModeScaleToFill;
-    
-    UILabel *lbl2 = [[UILabel alloc] initWithFrame:CGRectMake(self.scSlide.frame.size.width, 0, self.scSlide.frame.size.width, 250)];
-    lbl2.text = self.arTitles[1];
-    
-    [self.scSlide addSubview:p2];
-    [self.scSlide addSubview:lbl2];
-    
-    UIImageView *p3 = [[UIImageView alloc] initWithFrame:CGRectMake(2*self.scSlide.frame.size.width, 0, self.scSlide.frame.size.width, 250)];
-    p3.image = [UIImage imageNamed:self.arImages[2]];
-    p3.contentMode = UIViewContentModeScaleToFill;
-    
-    UILabel *lbl3 = [[UILabel alloc] initWithFrame:CGRectMake(2*self.scSlide.frame.size.width, 0, self.scSlide.frame.size.width, 250)];
-    lbl3.text = self.arTitles[2];
-    
-    [self.scSlide addSubview:p3];
-    [self.scSlide addSubview:lbl3];*/
-    
-    /*VTest *p1 = [[[NSBundle mainBundle] loadNibNamed:@"VTest" owner:self options:nil] objectAtIndex:0];
-    p1.frame = CGRectMake(self.scSlide.frame.size.width*0, 0, self.scSlide.frame.size.width, 250);
-    p1.imgPage.image = [UIImage imageNamed:self.arImages[0]];
-    p1.lblTitlePage.text = self.arTitles[0];
-    [self.scSlide addSubview:p1];
-    
-    VTest *p2 = [[[NSBundle mainBundle] loadNibNamed:@"VTest" owner:self options:nil] objectAtIndex:0];
-    p1.frame = CGRectMake(self.scSlide.frame.size.width*1, 0, self.scSlide.frame.size.width, 250);
-    p2.imgPage.image = [UIImage imageNamed:self.arImages[1]];
-    p2.lblTitlePage.text = self.arTitles[1];
-    [self.scSlide addSubview:p2];
-    
-    VTest *p3 = [[[NSBundle mainBundle] loadNibNamed:@"VTest" owner:self options:nil] objectAtIndex:0];
-    p3.frame = CGRectMake(self.scSlide.frame.size.width*2, 0, self.scSlide.frame.size.width, 250);
-    p3.imgPage.image = [UIImage imageNamed:self.arImages[2]];
-    p3.lblTitlePage.text = self.arTitles[2];
-    [self.scSlide addSubview:p3];*/
-    
     for (int i = 0; i < [self.arImages count]; i++) {
         VTest *page = [[[NSBundle mainBundle] loadNibNamed:@"VTest" owner:self options:nil] objectAtIndex:0];
         page.frame = CGRectMake(self.scSlide.frame.size.width*i, 0, self.scSlide.frame.size.width, self.scSlide.frame.size.height);

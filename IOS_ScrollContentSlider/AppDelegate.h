682ea55d97638bdeb4dfbb1f340e53275ceb1f11
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-21 12:35:28 -0600

    Initial Commit

---
+++ IOS_ScrollContentSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS_ScrollContentSlider
+//
+//  Created by Galileo Guzman on 21/01/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS_ScrollContentSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS_ScrollContentSlider
+//
+//  Created by Galileo Guzman on 21/01/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
